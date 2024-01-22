78f160f610708aaf8b59d85e968638c6be1afa2a
Adam Wei <weiadam@mit.edu>
2024-01-21 19:08:04 -0500

    improved memory usage of pkl->zarr script

--- data_generation/maze/convert_pkl_to_zarr.py
+++ data_generation/maze/convert_pkl_to_zarr.py
@@ -21,40 +21,71 @@ def main():
     |   |-- config.yaml
     ...
     
-    The script will store the generated zarr file in data_dir
+    The script will store the generated zarr file in data_dir.
+    It will only store trajectories from the subdirectories [start_idx, end_idx)
 
     Usage:
-    python convert_pkl_to_zarr.py --data_dir <path_to_data_dir>
+    python convert_pkl_to_zarr.py --data_dir <path_to_data_dir> \
+        --start_idx <start_idx> --end_idx <end_idx>
+    
     """
 
     # parse data_dir from command line
     parser = argparse.ArgumentParser()
     parser.add_argument('--data_dir', type=str)
+    parser.add_argument('--start_idx', type=int, default=None)
+    parser.add_argument('--end_idx', type=int, default=None)
     args = parser.parse_args()
-    assert args.data_dir is not None
-    config_path = os.path.join(args.data_dir, '0/config.yaml')
 
-    state = []
-    action = []
-    target = []
-    img = []
-    episode_ends = []
-    current_end = 0
-    
-    # Set up progress bar
-    num_pkl_files = len(next(os.walk(args.data_dir))[1])
+    assert args.data_dir is not None
+    data_dir = args.data_dir
+    if args.start_idx is None:
+        assert args.end_idx is None
+        args.start_idx = 0
+        args.end_idx = len(next(os.walk(args.data_dir))[1])
+    else:
+        assert args.end_idx is not None
+        assert args.start_idx < args.end_idx
+
+    config_path = os.path.join(args.data_dir, '0/config.yaml')
     with open(config_path, 'r') as yaml_file:
         cfg = yaml.load(yaml_file, Loader=yaml.FullLoader)
+
+    # Compute size of arrays and episode ends
+    episode_ends = []
+    total_points = 0
+    for dir_idx in tqdm(range(args.start_idx, args.end_idx)):
+        datapath = os.path.join(data_dir, str(dir_idx), 'maze_data.pkl')
+        data = pickle.load(open(datapath, 'rb'))
+
+        for maze_data in data:
+            trajectories = maze_data['trajectories']
+            for i in range(len(trajectories)):
+                trajectory = trajectories[i]
+                total_points += trajectory.shape[0]
+                episode_ends.append(total_points)
+    episode_ends = np.array(episode_ends)
+    print("Computed episode ends.")
+    
+    # Allocate memory for the arrays (these are VERY large)
+    state = np.zeros((total_points, 2)).astype(np.float32)
+    action = np.zeros((total_points, 2)).astype(np.float32)
+    target = np.zeros((total_points, 2)).astype(np.float32)
+    img = np.zeros((total_points, 
+                    cfg["image_size"][0],
+                    cfg["image_size"][1],
+                    3)).astype(np.float32)
+    print("Allocated memory for arrays.")
+
+    # Set up progress bar
+    num_pkl_files = args.end_idx - args.start_idx
     total_trajectories = num_pkl_files * \
         cfg['num_mazes_per_proc'] * cfg['num_trajectories_per_maze']
     pbar = tqdm(total=total_trajectories, position=0, leave=True)
 
-    for root, _, files in os.walk(args.data_dir):
-        # load data from file
-        if 'maze_data.pkl' not in files:
-            continue
-
-        datapath = os.path.join(root, 'maze_data.pkl')
+    traj_idx = 0
+    for dir_idx in range(args.start_idx, args.end_idx):
+        datapath = os.path.join(data_dir, str(dir_idx), 'maze_data.pkl')
         data = pickle.load(open(datapath, 'rb'))
         
         for maze_data in data:
@@ -68,28 +99,26 @@ def main():
                 goal = targets[i]
                 shifted_trajectory = \
                     np.concatenate([trajectory[1:, :], trajectory[-1:, :]], axis=0)
-                state.append(trajectory)
-                action.append(shifted_trajectory)
-                target.append(np.array([goal]*trajectory.shape[0]))
-                img.append(imgs[i])
-
-                current_end += trajectory.shape[0]
-                episode_ends.append(current_end)
                 
+                traj_start_idx = episode_ends[traj_idx]-trajectory.shape[0]
+                traj_end_idx = episode_ends[traj_idx]
+                state[traj_start_idx:traj_end_idx, :] = trajectory
+                action[traj_start_idx:traj_end_idx, :] = shifted_trajectory
+                target[traj_start_idx:traj_end_idx, :] = np.array([goal]*trajectory.shape[0])
+                img[traj_start_idx:traj_end_idx, :, :, :] = imgs[i]
+                
+                traj_idx += 1
                 pbar.update(1)
 
     pbar.close()
-    
-    # Convert data to numpy arrays
-    state = np.concatenate(state, axis=0).astype(np.float32)
-    action = np.concatenate(action, axis=0).astype(np.float32)
-    target = np.concatenate(target, axis=0).astype(np.float32)
-    img = np.concatenate(img, axis=0).astype(np.float32)
-    episode_ends = np.array(episode_ends)
     print("Concatenated data.")
 
     # Create zarr file and group structure
-    zarr_path = os.path.join(args.data_dir, 'maze_image_dataset.zarr')
+    if args.start_idx is None:
+        zarr_path = os.path.join(args.data_dir, 'maze_image_dataset.zarr')
+    else:
+        zarr_path = os.path.join(args.data_dir, 
+            f'maze_image_dataset_{args.start_idx}_{args.end_idx}.zarr')
     root = zarr.open_group(zarr_path, mode='w')
     data_dir = root.create_group('data')
     meta_dir = root.create_group('meta')
@@ -109,7 +138,8 @@ def main():
     data_dir.create_dataset('img', data=img, chunks=image_chunk_size, dtype='f4')
     print("Stored img data.")
     meta_dir.create_dataset('episode_ends', data=episode_ends)
-    print("Stored episode_ends data. All done.")
+    print("Stored episode_ends data.")
+    print("All done.")
 
 if __name__ == '__main__':
     main()
