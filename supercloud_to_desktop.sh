cfb4d9c05b4dc6315bc71bdb052f357e6965d416
agarwal-abhinav <abhiagarwal1712@gmail.com>
2025-07-17 11:57:42 -0400

    changes made to dit basic configs

--- supercloud_to_desktop.sh
+++ supercloud_to_desktop.sh
@@ -3,14 +3,14 @@
 # Source directory on your computer
 # SOURCE_DIR="outputs/diffusion_experiments/cartpole_controller_noise_100/partially_observable_h_50_pred_8_ablation/3_traj/"
 # SOURCE_DIR="outputs/diffusion_experiments/random_system_seed_0_controller_noise_0.5/partially_observable_ss/0_mean_50_context/checkpoints/"
-SOURCE_DIR="data/outputs/grasp_two_bins/constant_model_size_frozen_encoder/2_frozen/5_obs/checkpoints/"
-# SOURCE_DIR="data/outputs/"
+# SOURCE_DIR="data/outputs/grasp_two_bins/constant_model_size_frozen_encoder/2_frozen/5_obs/checkpoints/"
+SOURCE_DIR="data/outputs/planar_pushing/diffusion_transformer_training/context_length_exp_adam_data/"
 
 # Target directory on the target computer
 TARGET_USER="aagarwal2"
 TARGET_HOST="txe1-login.mit.edu"
-TARGET_DIR="/home/gridsan/aagarwal2/RLG/gcs-diffusion/data/outputs/grasp_two_bins/constant_model_size_frozen_encoder/2_frozen/5_obs/checkpoints/latest.ckpt"
-# TARGET_DIR="/home/gridsan/aagarwal2/RLG/gcs-diffusion/data/outputs/context_length_exp_adam_data_constant_model_size_init_encoder/2_encoder_init/12_obs/normalizer.pt"
+# TARGET_DIR="/home/gridsan/aagarwal2/RLG/gcs-diffusion/data/outputs/grasp_two_bins/constant_model_size_frozen_encoder/2_frozen/5_obs/checkpoints/latest.ckpt"
+TARGET_DIR="/home/gridsan/aagarwal2/RLG/gcs-diffusion/data/outputs/planar_pushing/diffusion_transformer_training/context_length_exp_adam_data/5_obs"
 # TARGET_DIR="/home/gridsan/aagarwal2/RLG/gcs-diffusion/data/outputs/context_length_exp_adam_data_constant_model_size_low_pass"
 
 # TARGET_DIR="/home/gridsan/aagarwal2/RLG/diffusion-search-learning/outputs/diffusion_experiments/random_system_seed_0_controller_noise_0.5/partially_observable_ss/0_mean_50_context/checkpoints/latest.ckpt"
