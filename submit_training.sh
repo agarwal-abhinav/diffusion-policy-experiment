af303a654861830eca29e987e771827d63fb389c
Abhinav Agarwal <abhiagarwal1712@gmail.com>
2025-07-24 15:58:57 -0400

    some grasp task configs

--- submit_training.sh
+++ submit_training.sh
@@ -24,13 +24,13 @@ echo "[submit_training.sh] Running training code..."
 echo "[submit_training.sh] Date: $DATE"
 echo "[submit_training.sh] Time: $TIME"
 
-# CONFIG_DIR=config/planar_pushing/context_length_exp_adam_data_constant_model_size_init_encoder/2_encoder_freeze_then_resume/
-# CONFIG_NAME=16_obs.yaml
-# HYDRA_RUN_DIR=data/outputs/context_length_exp_adam_data_constant_model_size_init_encoder/2_encoder_freeze_then_resume/16_obs
+CONFIG_DIR=config/planar_pushing/context_length_exp_adam_data_resnet_plus_transformer/cls_token_only_causal/
+CONFIG_NAME=5_obs.yaml
+HYDRA_RUN_DIR=data/outputs/context_length_exp_adam_data_resnet_plus_transformer/cls_token_only_causal/5_obs
 
-CONFIG_DIR=config/grasp_two_bins/resnet_plus_transformer/cls_token_only/
-CONFIG_NAME=16_obs.yaml
-HYDRA_RUN_DIR=data/outputs/grasp_two_bins/resnet_plus_transformer/cls_token_only/16_obs
+# CONFIG_DIR=config/grasp_two_bins/resnet_plus_transformer/cls_token_only/
+# CONFIG_NAME=5_obs.yaml
+# HYDRA_RUN_DIR=data/outputs/grasp_two_bins/resnet_plus_transformer/cls_token_only/5_obs
 
 python train.py --config-dir=$CONFIG_DIR --config-name=$CONFIG_NAME \
 	hydra.run.dir=$HYDRA_RUN_DIR
