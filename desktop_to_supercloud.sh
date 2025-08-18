7307ab9284564eb292935a459437d5a7baa0eb36
agarwal-abhinav <abhiagarwal1712@gmail.com>
2025-08-17 23:02:17 -0400

    some files for simple planar pushing training added

--- desktop_to_supercloud.sh
+++ desktop_to_supercloud.sh
@@ -2,12 +2,12 @@
 
 # Source directory on your computer
 # SOURCE_DIR="data/sim_sim_tee_data_carbon_large.zarr"
-SOURCE_DIR="data/outputs/grasp_two_bins/constant_model_size/2_obs/latest_ckpt_encoder.pth"
+SOURCE_DIR="data/canonical_planar_pushing_lc/lc_block_push_combined.zarr"
 
 # Target directory on the target computer
 TARGET_USER="aagarwal2"
 TARGET_HOST="txe1-login.mit.edu"
-TARGET_DIR="/home/gridsan/aagarwal2/RLG/gcs-diffusion/data/outputs/grasp_two_bins/constant_model_size/2_obs/"
+TARGET_DIR="/home/gridsan/aagarwal2/RLG/gcs-diffusion/data/canonical_planar_pushing_lc/"
 
 # Rsync command with -avh flags
 rsync -avh "$SOURCE_DIR" "$TARGET_USER@$TARGET_HOST:$TARGET_DIR" 
