4869e3e22f4b4dc264403e38e698333af375bc92
agarwal-abhinav <abhiagarwal1712@gmail.com>
2025-08-31 21:27:34 -0400

    configs added for resnet with more channels and some trainings for grasp task

--- desktop_to_supercloud.sh
+++ desktop_to_supercloud.sh
@@ -2,13 +2,13 @@
 
 # Source directory on your computer
 # SOURCE_DIR="data/sim_sim_tee_data_carbon_large.zarr"
-# SOURCE_DIR="data/diffusion_experiments/grasp_two_bins/two_bins_flat_data_2_right.zarr"
-SOURCE_DIR="data/pretrained_models/mc3_18.pth"
+SOURCE_DIR="data/diffusion_experiments/grasp_two_bins/two_bins_flat_data_2_right_same_return_same_center_1_second_total.zarr"
+# SOURCE_DIR="data/pretrained_models/robomimic_resnet18_spatialsoftmax_gn.pth"
 
 # Target directory on the target computer
 TARGET_USER="aagarwal2"
 TARGET_HOST="txe1-login.mit.edu"
-TARGET_DIR="/home/gridsan/aagarwal2/RLG/gcs-diffusion/data/pretrained_models/"
+TARGET_DIR="/home/gridsan/aagarwal2/RLG/gcs-diffusion/data/diffusion_experiments/grasp_two_bins/"
 
 # Rsync command with -avh flags
 rsync -avh "$SOURCE_DIR" "$TARGET_USER@$TARGET_HOST:$TARGET_DIR" 
--- vid_image_creator.py
+++ vid_image_creator.py
@@ -7,23 +7,23 @@ zarr_path = "data/planar_pushing_cotrain/sim_sim_tee_data_carbon_large.zarr"
 # open zarr group
 root = zarr.open(zarr_path, mode="r")
 
-# navigate to data/overhead_camera
-frames = root["data"]["overhead_camera"]  # usually shape (T, H, W, C)
+# navigate to data/wrist_camera
+frames = root["data"]["wrist_camera"]  # usually shape (T, H, W, C)
 
 # select first 100 frames
-frames100 = frames[:100]  # numpy-like slicing
+frames100 = frames[:300]  # numpy-like slicing
 
 # save first frame as PNG
-first_frame = frames100[0]
-imageio.imwrite("overhead_first.png", first_frame)
+first_frame = frames100[124]
+imageio.imwrite("wrist_first.png", first_frame)
 
 # write all 100 frames to MP4
-out_path = "overhead_first100.mp4"
+out_path = "wrist_first100.mp4"
 fps = 5  # frames per second
 
 with imageio.get_writer(out_path, fps=fps, codec="libx264") as writer:
     for frame in frames100:
         writer.append_data(frame)
 
-print("Saved first frame as overhead_first.png")
+print("Saved first frame as wrist_first.png")
 print(f"Saved first 100 frames as {out_path}")
