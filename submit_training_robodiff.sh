335490971523928a078e5169bbb4b155766aa4c3
Abhinav Agarwal <abhiagarwal1712@gmail.com>
2025-09-07 21:34:26 -0400

    submit script for files using older versions of packages

--- submit_training_robodiff.sh
+++ submit_training_robodiff.sh
@@ -7,9 +7,11 @@
 echo "[submit_training.sh] Loading modules and virtual environment"
 source /etc/profile
 module load anaconda/2023b
+source activate robodiff
 
+export PYTHONNOUSERSITE=True
 # Assume current directory is gcs-diffusion
-source .robodiff/bin/activate || echo "Training with anaconda/2023b module instead of venv"
+# source .robodiff/bin/activate || echo "Training with anaconda/2023b module instead of venv"
 
 # Set wandb to offline since Supercloud has no internet access
 echo "[submit_training.sh] Setting wandb to offline"
@@ -18,15 +20,15 @@ wandb offline
 # Export date, time, environment variables
 DATE=`date +"%Y.%m.%d"`
 TIME=`date +"%H.%M.%S"`
-HYDRA_FULL_ERROR=1
+export HYDRA_FULL_ERROR=1
 
 echo "[submit_training.sh] Running training code..."
 echo "[submit_training.sh] Date: $DATE"
 echo "[submit_training.sh] Time: $TIME"
 
-CONFIG_DIR=config/planar_pushing/context_length_exp_adam_data_resnet_plus_transformer/all_tokens_cms/
-CONFIG_NAME=12_obs.yaml
-HYDRA_RUN_DIR=data/outputs/context_length_exp_adam_data_resnet_plus_transformer/all_tokens_cms/12_obs
+CONFIG_DIR=config/robomimic/square/resnet18_init/
+CONFIG_NAME=16_obs.yaml
+HYDRA_RUN_DIR=data/outputs/robomimic/square/resnet18_init/16_obs
 
 # CONFIG_DIR=config/planar_pushing/context_length_exp_adam_data_constant_model_size/robomimic_resnet18_freeze/
 # CONFIG_NAME=1_obs.yaml
