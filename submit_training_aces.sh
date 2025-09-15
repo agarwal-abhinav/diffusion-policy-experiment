1a0601f146478ce757fb34a7b0d0f55bb6acd740
Abhinav Agarwal <u.aa336018@alogin1.cluster>
2025-09-15 15:47:42 -0500

    training changes

--- submit_training_aces.sh
+++ submit_training_aces.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 ##NECESSARY JOB SPECIFICATIONS
-#SBATCH --job-name=Variable_length_sprinkle_resnet_init_12_obs #Set the job name to Example_SNMC_GPU
+#SBATCH --job-name=Variable_length_resnet_init_10_obs #Set the job name to Example_SNMC_GPU
 #SBATCH --time=15:30:00 #Set the wall clock limit to 15hr 30min
 #SBATCH --nodes=1 #Request 1 nodes
-#SBATCH --ntasks-per-node=10 #Request 16 tasks/cores per node
-#SBATCH --mem=35G #Request 35 (488GB) per node
+#SBATCH --ntasks-per-node=8 #Request 16 tasks/cores per node
+#SBATCH --mem=40G #Request 40 (488GB) per node
 #SBATCH --output=submit_training_aces.sh.log-%j #Redirect stdout/err to file
 #SBATCH --partition=gpu #Specify partition to submit job to
 #SBATCH --gres=gpu:h100:1 #Specify GPU(s) per node, 1 H100 GPUs
@@ -20,8 +20,8 @@ source /scratch/user/u.aa336018/RLG/python_environments/aces_diff_training_h100/
 
 wandb offline 
 
-CONFIG_DIR=config/planar_pushing/context_length_exp_adam_data_variable_training/random_sprinkle_2_to_obs_resnet_init
-CONFIG_NAME=12_obs.yaml
-HYDRA_RUN_DIR=data/outputs/context_length_exp_adam_data_variable_training/random_sprinkle_2_to_obs_resnet_init/12_obs
+CONFIG_DIR=config/planar_pushing/context_length_exp_adam_data_variable_training/random_2_to_obs_resnet_init
+CONFIG_NAME=10_obs.yaml
+HYDRA_RUN_DIR=data/outputs/context_length_exp_adam_data_variable_training/random_2_to_obs_resnet_init/10_obs
 
 python train.py --config-dir=$CONFIG_DIR --config-name=$CONFIG_NAME hydra.run.dir=$HYDRA_RUN_DIR
