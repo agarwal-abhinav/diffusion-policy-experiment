1a8c2e49495b6b0bd08adbb2a1efcf561e783e03
Abhinav Agarwal <u.aa336018@alogin3.cluster>
2025-12-01 12:19:07 -0600

    more configs added on aces for long context training

--- submit_training_aces.sh
+++ submit_training_aces.sh
@@ -2,13 +2,13 @@
 
 ##NECESSARY JOB SPECIFICATIONS
 #SBATCH --job-name=Variable_length_constant_obs_steps_resnet_init_12_obs #Set the job name to Example_SNMC_GPU
-#SBATCH --time=20:00:00 #Set the wall clock limit to 15hr 30min
+#SBATCH --time=40:00:00 #Set the wall clock limit to 15hr 30min
 #SBATCH --nodes=1 #Request 1 nodes
 #SBATCH --ntasks-per-node=8 #Request 16 tasks/cores per node
-#SBATCH --mem=40G #Request 40 (40GB) per node
+#SBATCH --mem=50G #Request 50 (50GB) per node
 #SBATCH --output=submit_training_aces.sh.log-%j #Redirect stdout/err to file
 #SBATCH --partition=gpu #Specify partition to submit job to
-#SBATCH --gres=gpu:a30:1 #Specify GPU(s) per node, 1 H100 GPUs
+#SBATCH --gres=gpu:h100:1 #Specify GPU(s) per node, 1 H100 GPUs
 
 ##OPTIONAL JOB SPECIFICATIONS 
 ##SBATCH --mail-type=ALL #Send email on all job events
@@ -28,8 +28,8 @@ wandb offline
 ##CONFIG_NAME=16_obs.yaml
 ##HYDRA_RUN_DIR=data/outputs/context_length_exp_adam_data_resnet_plus_transformer/all_tokens_limited_attention/upto_12/16_obs
 
-CONFIG_DIR=config/grasp_two_bins_flat_green/same_middle_same_return_green/basic_training
-CONFIG_NAME=22_obs.yaml
-HYDRA_RUN_DIR=data/outputs/grasp_two_bins_flat_green/same_middle_same_return_green/basic_training/22_obs
+CONFIG_DIR=config/long_context_planar_pushing/two_modes/unet_cross_attention/constant_obs_steps/
+CONFIG_NAME=60_obs.yaml
+HYDRA_RUN_DIR=data/outputs/long_context_planar_pushing/two_modes/unet_cross_attention/constant_obs_steps/60_obs
 
 python train.py --config-dir=$CONFIG_DIR --config-name=$CONFIG_NAME hydra.run.dir=$HYDRA_RUN_DIR
