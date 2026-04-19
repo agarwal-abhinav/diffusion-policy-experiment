82324668e027f1f333223d4b2e50564476474131
Abhinav Agarwal <abhiagarwal1712@gmail.com>
2026-04-19 17:16:59 -0400

    changes to training submission

--- submit_training.sh
+++ submit_training.sh
@@ -24,57 +24,17 @@ echo "[submit_training.sh] Running training code..."
 echo "[submit_training.sh] Date: $DATE"
 echo "[submit_training.sh] Time: $TIME"
 
-# CONFIG_DIR=config/planar_pushing/context_length_exp_adam_data_variable_training/random_sprinkle_2_to_obs_resnet_init/
-# CONFIG_NAME=12_obs.yaml
-# HYDRA_RUN_DIR=data/outputs/context_length_exp_adam_data_variable_training/random_sprinkle_2_to_obs_resnet_init/12_obs
+# CONFIG_DIR=config/iros/long_context_planar_pushing/data_experiments/unet_cross_attention/two_modes/data_96/mode_4_0
+# CONFIG_NAME=8_obs.yaml 
+# HYDRA_RUN_DIR=data/outputs/iros/long_context_planar_pushing/data_experiments/unet_cross_attention/two_modes/data_96/mode_4_0/8_obs
 
-# CONFIG_DIR=config/planar_pushing/diffusion_transformer_training/context_length_exp_adam_data/robomimic_resnet18_init/
-# CONFIG_NAME=1_obs.yaml
-# HYDRA_RUN_DIR=data/outputs/diffusion_transformer_training/context_length_exp_adam_data/robomimic_resnet18_init/1_obs
-
-# CONFIG_DIR=config/planar_pushing/context_length_exp_adam_data_constant_model_size/
-# CONFIG_NAME=5_obs.yaml
-# HYDRA_RUN_DIR=data/outputs/context_length_exp_adam_data_constant_model_size/5_obs
-
-# CONFIG_DIR=config/grasp_two_bins_flat/attention_training/random_4_to_obs
-# CONFIG_NAME=22_obs.yaml
-# HYDRA_RUN_DIR=data/outputs/grasp_two_bins_flat/attention_training/random_4_to_obs/22_obs
-
-# CONFIG_DIR=config/grasp_two_bins_flat_green/attention_training/constant_obs_steps_baseline/
-# CONFIG_NAME=30_obs.yaml
-# HYDRA_RUN_DIR=data/outputs/grasp_two_bins_flat_green/attention_training/constant_obs_steps_baseline/30_obs
-
-# CONFIG_DIR=config/canonical_planar_pushing/initial_training/
-# CONFIG_NAME=20_obs_h_32.yaml
-# HYDRA_RUN_DIR=data/outputs/canonical_planar_pushing/initial_training/20_obs_h_32
-
-# CONFIG_DIR=config/long_context_planar_pushing/two_modes/unet_film/0_via_mirror
-# CONFIG_NAME=30_obs.yaml
-# HYDRA_RUN_DIR=data/outputs/long_context_planar_pushing/two_modes/unet_film/0_via_mirror/30_obs_retry
-
-# CONFIG_DIR=config/long_context_planar_pushing/two_modes/unet_cross_attention/constant_obs_steps_0_mirror
-# CONFIG_NAME=5_obs.yaml
-# HYDRA_RUN_DIR=data/outputs/long_context_planar_pushing/two_modes/unet_cross_attention/constant_obs_steps_0_mirror_25_each/5_obs
-
-CONFIG_DIR=config/iros/planar_pushing/resnet18_init/unet_cross_attention/data_160
-CONFIG_NAME=16_obs.yaml
-HYDRA_RUN_DIR=data/outputs/iros/planar_pushing/resnet18_init/unet_cross_attention/data_160/16_obs
-
-# CONFIG_DIR=config/long_context_planar_pushing/single_mode/unet_cross_attention/4_original/constant_obs_steps
-# CONFIG_NAME=5_obs.yaml
-# HYDRA_RUN_DIR=data/outputs/long_context_planar_pushing/single_mode/unet_film/4_original/constant_obs_steps_25_data/5_obs
-
-# CONFIG_DIR=config/iros/long_context_planar_pushing/data_experiments/unet_cross_attention/single_mode/data_48/mode_4
-# CONFIG_NAME=8_obs.yaml
-# HYDRA_RUN_DIR=data/outputs/iros/long_context_planar_pushing/data_experiments/unet_cross_attention/single_mode/data_48/mode_4/8_obs
-
-# CONFIG_DIR=config/iros/long_context_planar_pushing/data_experiments/unet_cross_attention/two_modes/data_72/mode_4_0
-# CONFIG_NAME=4_obs.yaml
-# HYDRA_RUN_DIR=data/outputs/iros/long_context_planar_pushing/data_experiments/unet_cross_attention/two_modes/data_72/mode_4_0/4_obs
-
-# CONFIG_DIR=config/iros/long_context_grasping/data_experiments/unet_cross_attention/two_modes_diff_center_diff_return/data_48/
+# CONFIG_DIR=config/iros/long_context_grasping/data_experiments/unet_cross_attention/two_modes_diff_center_diff_return/data_48
 # CONFIG_NAME=8_obs.yaml 
 # HYDRA_RUN_DIR=data/outputs/iros/long_context_grasping/data_experiments/unet_cross_attention/two_modes_diff_center_diff_return/data_48/8_obs
 
+CONFIG_DIR=config/iros/planar_pushing/variable_context/data_80
+CONFIG_NAME=variable_progressive_resnet_full.yaml 
+HYDRA_RUN_DIR=data/outputs/iros/planar_pushing/variable_context/data_80/variable_progressive_resnet_full
+
 python train.py --config-dir=$CONFIG_DIR --config-name=$CONFIG_NAME \
-	hydra.run.dir=$HYDRA_RUN_DIR
+	hydra.run.dir=$HYDRA_RUN_DIR logging.mode="offline"
